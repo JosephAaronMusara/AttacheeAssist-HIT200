139d9e475ab021142f80195f39bb5a72f2743444
Joseph Aaron Musara <111688993+JosephAaronMusara@users.noreply.github.com>
2024-06-28 12:51:00 +0200

    remove secrete key

--- job/settings.py
+++ job/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'etdq)uvq=t0rc&ams5_ovn6w8bcwknjj0u97*(#n^(76x*+dr1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
