4474b6ab030de718227d21222741dd797eeff1e8
Joseph Aaron Musara <111688993+JosephAaronMusara@users.noreply.github.com>
2024-06-28 12:48:36 +0200

    Update settings.py
    Fixed SMTP secret leak

--- job/settings.py
+++ job/settings.py
@@ -206,7 +206,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hitattacheeassist@gmail.com'
-EMAIL_HOST_PASSWORD = 'kwyl ocnh zchs siqy'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
